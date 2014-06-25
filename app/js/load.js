456e15e869ba6b0b88876d6caa7b396bf9cde7a3
Vasari <nmccallum7@gmail.com>
2014-06-25 12:46:55 +1000

    Slimmed down loading a LOT.

--- app/js/load.js
+++ app/js/load.js
@@ -1,4 +1,7 @@
-(function(){
+(function() {
+    // This converts url parameters into a usable object. I think it's only used
+    // in the registration process when someone clicks a referral link (which
+    // has a code in the url as a parameter.)
     var l = window.location;
     var query = {};
     var vars = l.hash.substring(1).split('&');
@@ -15,6 +18,8 @@
         l.hash = '';
     }
 
+
+    // Make sure the pulser is hidden.
     var p = document.getElementById("pulsing");
     if(p.className.indexOf('hidden') < 0) {
         p.className += ' hidden';
@@ -27,462 +32,105 @@
         allowRollup: false,
         combine: false
     });
-    var urlBuilder = function(file) {
-        return host + file + '?' + Date.now().toString();
-    };
 
-    loader.addModule({
-        name: "smd",
-        type: "js",
-        fullpath: urlBuilder("smd.js"),
-        requires : ["yahoo"]
-    });
-    loader.addModule({
-        name: "rpc",
-        type: "js",
-        fullpath: urlBuilder("rpc.js"),
-        requires : ["yahoo","dom","connection","get","json"]
-    });
-    loader.addModule({
-        name: "library",
-        type: "js",
-        fullpath: urlBuilder("library.js"),
-        requires : ["yahoo","dom"]
-    });
-    loader.addModule({
-        name: "game",
-        type: "js",
-        fullpath: urlBuilder("game.js"),
-        requires : ["event","library","pulse","rpc","smd"]
-    });
-    loader.addModule({
-        name: "about",
-        type: "js",
-        fullpath: urlBuilder("about.js"),
-        requires : ["container","event","game"]
-    });
-    loader.addModule({
-        name: "announce",
-        type: "js",
-        fullpath: urlBuilder("announce.js"),
-        requires : ["container","game"]
-    });
-    loader.addModule({
-        name: "speciesDesigner",
-        type: "js",
-        fullpath: urlBuilder("speciesDesigner.js"),
-        requires : ["dragdrop","game","slider"]
-    });
-    loader.addModule({
-        name: "createSpecies",
-        type: "js",
-        fullpath: urlBuilder("createSpecies.js"),
-        requires : ["game","speciesDesigner"]
-    });
-    loader.addModule({
-        name: "createEmpire",
-        type: "js",
-        fullpath: urlBuilder("createEmpire.js"),
-        requires : ["createSpecies"]
-    });
-    loader.addModule({
-        name: "login",
-        type: "js",
-        fullpath: urlBuilder("login.js"),
-        requires : ["container","createEmpire","game"]
-    });
-    loader.addModule({
-        name: "mapper",
-        type: "js",
-        fullpath: urlBuilder("mapper.js"),
-        requires : ["animation","dom","dragdrop","game","selector"]
-    });
-    loader.addModule({
-        name: "mapStar",
-        type: "js",
-        fullpath: urlBuilder("mapStar.js"),
-        requires : ["event-delegate","game","mapper","selector"]
-    });
-    //buildings
-    loader.addModule({
-        name: "building",
-        type: "js",
-        fullpath: urlBuilder("building.js"),
-        requires : ["game","paginator","selector","tabview"]
-    });
-    loader.addModule({
-        name: "archaeology",
-        type: "js",
-        fullpath: urlBuilder("buildingArchaeology.js"),
-        requires : ["animation","building","dragdrop"]
-    });
-    loader.addModule({
-        name: "blackholegenerator",
-        type: "js",
-        fullpath: urlBuilder("buildingBlackHoleGenerator.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "capitol",
-        type: "js",
-        fullpath: urlBuilder("buildingCapitol.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "development",
-        type: "js",
-        fullpath: urlBuilder("buildingDevelopment.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "distributioncenter",
-        type: "js",
-        fullpath: urlBuilder("buildingDistributionCenter.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "embassy",
-        type: "js",
-        fullpath: urlBuilder("buildingEmbassy.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "energyreserve",
-        type: "js",
-        fullpath: urlBuilder("buildingEnergyReserve.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "entertainment",
-        type: "js",
-        fullpath: urlBuilder("buildingEntertainment.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "foodreserve",
-        type: "js",
-        fullpath: urlBuilder("buildingFoodReserve.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "geneticslab",
-        type: "js",
-        fullpath: urlBuilder("buildingGeneticsLab.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "intelligence",
-        type: "js",
-        fullpath: urlBuilder("buildingIntelligence.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "inteltraining",
-        type: "js",
-        fullpath: urlBuilder("buildingIntelTraining.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "libraryofjith",
-        type: "js",
-        fullpath: urlBuilder("buildingLibraryOfJith.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "mayhemtraining",
-        type: "js",
-        fullpath: urlBuilder("buildingMayhemTraining.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "mercenariesguild",
-        type: "js",
-        fullpath: urlBuilder("buildingMercenariesGuild.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "miningministry",
-        type: "js",
-        fullpath: urlBuilder("buildingMiningMinistry.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "missioncommand",
-        type: "js",
-        fullpath: urlBuilder("buildingMissionCommand.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "network19",
-        type: "js",
-        fullpath: urlBuilder("buildingNetwork19.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "observatory",
-        type: "js",
-        fullpath: urlBuilder("buildingObservatory.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "oracleofanid",
-        type: "js",
-        fullpath: urlBuilder("buildingOracleOfAnid.js"),
-        requires : ["building","mapStar"]
-    });
-    loader.addModule({
-        name: "orestorage",
-        type: "js",
-        fullpath: urlBuilder("buildingOreStorage.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "park",
-        type: "js",
-        fullpath: urlBuilder("buildingPark.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "planetarycommand",
-        type: "js",
-        fullpath: urlBuilder("buildingPlanetaryCommand.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "politicstraining",
-        type: "js",
-        fullpath: urlBuilder("buildingPoliticsTraining.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "security",
-        type: "js",
-        fullpath: urlBuilder("buildingSecurity.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "shipyard",
-        type: "js",
-        fullpath: urlBuilder("buildingShipyard.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "spaceport",
-        type: "js",
-        fullpath: urlBuilder("buildingSpacePort.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "spacestationlab",
-        type: "js",
-        fullpath: urlBuilder("buildingSpaceStationLab.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "subspacesupplydepot",
-        type: "js",
-        fullpath: urlBuilder("buildingSubspaceSupplyDepot.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "templeofthedrajilites",
-        type: "js",
-        fullpath: urlBuilder("buildingTempleOfTheDrajilites.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "thefttraining",
-        type: "js",
-        fullpath: urlBuilder("buildingTheftTraining.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "themepark",
-        type: "js",
-        fullpath: urlBuilder("buildingThemePark.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "thedillonforge",
-        type: "js",
-        fullpath: urlBuilder("buildingTheDillonForge.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "trade",
-        type: "js",
-        fullpath: urlBuilder("buildingTradeMinistry.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "transporter",
-        type: "js",
-        fullpath: urlBuilder("buildingTransporter.js"),
-        requires : ["building","trade"]
-    });
-    loader.addModule({
-        name: "wasteexchanger",
-        type: "js",
-        fullpath: urlBuilder("buildingWasteExchanger.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "wasterecycling",
-        type: "js",
-        fullpath: urlBuilder("buildingWasteRecycling.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "waterstorage",
-        type: "js",
-        fullpath: urlBuilder("buildingWaterStorage.js"),
-        requires : ["building"]
-    });
-    //modules
-    loader.addModule({
-        name: "parliament",
-        type: "js",
-        fullpath: urlBuilder("moduleParliament.js"),
-        requires : ["building"]
-    });
-    loader.addModule({
-        name: "policestation",
-        type: "js",
-        fullpath: urlBuilder("modulePoliceStation.js"),
-        requires : ["security"]
-    });
-    loader.addModule({
-        name: "stationcommand",
-        type: "js",
-        fullpath: urlBuilder("moduleStationCommand.js"),
-        requires : ["planetarycommand"]
-    });
-    //
-    loader.addModule({
-        name: "mapPlanet",
-        type: "js",
-        fullpath: urlBuilder("mapPlanet.js"),
-        requires : [
-            //buildings
-            "archaeology",
-            "blackholegenerator",
-            "capitol",
-            "development",
-            "distributioncenter",
-            "embassy",
-            "energyreserve",
-            "entertainment",
-            "foodreserve",
-            "geneticslab",
-            "intelligence",
-            "inteltraining",
-            "libraryofjith",
-            "mayhemtraining",
-            "mercenariesguild",
-            "miningministry",
-            "missioncommand",
-            "network19",
-            "observatory",
-            "oracleofanid",
-            "orestorage",
-            "park",
-            "planetarycommand",
-            "politicstraining",
-            "security",
-            "shipyard",
-            "spaceport",
-            "spacestationlab",
-            "subspacesupplydepot",
-            "templeofthedrajilites",
-            "thefttraining",
-            "themepark",
-            "thedillonforge",
-            "trade",
-            "transporter",
-            "waterstorage",
-            "wasteexchanger",
-            "wasterecycling",
-            //modules
-            "parliament",
-            "policestation",
-            "stationcommand",
-            //helpers
-            "event-delegate",
-            "event-mouseenter",
-            "mapper",
-            "selector"
-        ]
-    });
-    loader.addModule({
-        name: "textboxList",
-        type: "js",
-        fullpath: urlBuilder("textboxList.js"),
-        requires : ["autocomplete","library"]
-    });
-    loader.addModule({
-        name: "messaging",
-        type: "js",
-        fullpath: urlBuilder("messaging.js"),
-        requires : ["datasource","event-delegate","game","paginator","textboxList"]
-    });
-    loader.addModule({
-        name: "essentia",
-        type: "js",
-        fullpath: urlBuilder("essentia.js"),
-        requires : ["container","game","tabview"]
-    });
-    loader.addModule({
-        name: "invite",
-        type: "js",
-        fullpath: urlBuilder("invite.js"),
-        requires : ["container","game"]
-    });
-    loader.addModule({
-        name: "profile",
-        type: "js",
-        fullpath: urlBuilder("profile.js"),
-        requires : ["container","game","tabview","speciesDesigner"]
-    });
-    loader.addModule({
-        name: "stats",
-        type: "js",
-        fullpath: urlBuilder("stats.js"),
-        requires : ["container","datasource","datatable","game","selector","tabview"]
-    });
-    loader.addModule({
-        name: "pulse",
-        type: "js",
-        fullpath: urlBuilder("pulse.js"),
-        requires : ["container","dom","event"]
-    });
-    loader.addModule({
-        name: "info",
-        type: "js",
-        fullpath: urlBuilder("info.js"),
-        requires : ["container","dom","game"]
-    });
-    loader.addModule({
-        name: "notify",
-        type: "js",
-        fullpath: urlBuilder("notify.js"),
-        requires : ["container","dom","game"]
-    });
-    loader.addModule({
-        name: "captcha",
-        type: "js",
-        fullpath: urlBuilder("captcha.js"),
-        requires : ["container","dom","game"]
-    });
-    /*add after requirements*/
-    loader.addModule({
-        name: "gameMenu",
-        type: "js",
-        fullpath: urlBuilder("menu.js"),
-        // invite has to come before essentia in order for the friend invite button to work
-        requires : ["about","info","invite","essentia","messaging","menu","profile","stats"]
-    });
-    loader.require("gameMenu","announce","info","logger","login","mapPlanet","mapStar","notify","captcha");
+    loader.require([
+        "autocomplete",
+        "logger",
+        "yahoo",
+        "dom",
+        "connection",
+        "get",
+        "json",
+        "event",
+        "container",
+        "dragdrop",
+        "slider",
+        "animation",
+        "selector",
+        "event-delegate",
+        "event-mouseenter",
+        "paginator",
+        "tabview",
+        "menu"
+    ]);
+
     loader.onSuccess = function(o) {
+        // RPC and core stuff
+        require('js/smd');
+        require('js/rpc');
+        require('js/library');
+        require('js/game');
+
+        // Misc?
+        require('js/about');
+        require('js/announce');
+
+        // Empire management and star map
+        require('js/speciesDesigner');
+        require('js/createSpecies');
+        require('js/createEmpire');
+        require('js/login');
+        require('js/mapper');
+        require('js/mapStar');
+
+        // Buildings
+        require('js/building');
+        require('js/buildingArchaeology');
+        require('js/buildingBlackHoleGenerator');
+        require('js/buildingCapitol');
+        require('js/buildingDevelopment');
+        require('js/buildingDistributionCenter');
+        require('js/buildingEmbassy');
+        require('js/buildingEnergyReserve');
+        require('js/buildingEntertainment');
+        require('js/buildingFoodReserve');
+        require('js/buildingGeneticsLab');
+        require('js/buildingIntelligence');
+        require('js/buildingIntelTraining');
+        require('js/buildingLibraryOfJith');
+        require('js/buildingMayhemTraining');
+        require('js/buildingMercenariesGuild');
+        require('js/buildingMiningMinistry');
+        require('js/buildingMissionCommand');
+        require('js/buildingNetwork19');
+        require('js/buildingObservatory');
+        require('js/buildingOracleOfAnid');
+        require('js/buildingOreStorage');
+        require('js/buildingPark');
+        require('js/buildingPlanetaryCommand');
+        require('js/buildingPoliticsTraining');
+        require('js/buildingSecurity');
+        require('js/buildingShipyard');
+        require('js/buildingSpacePort');
+        require('js/buildingSpaceStationLab');
+        require('js/buildingSubspaceSupplyDepot');
+        require('js/buildingTempleOfTheDrajilites');
+        require('js/buildingTheftTraining');
+        require('js/buildingThemePark');
+        require('js/buildingTheDillonForge');
+        require('js/buildingTradeMinistry');
+        require('js/buildingTransporter');
+        require('js/buildingWasteExchanger');
+        require('js/buildingWasteRecycling');
+        require('js/buildingWaterStorage');
+        require('js/moduleParliament');
+        require('js/modulePoliceStation');
+        require('js/moduleStationCommand');
+        require('js/mapPlanet');
+
+        // Menu stuff
+        require('js/textboxList');
+        require('js/messaging');
+        require('js/essentia');
+        require('js/invite');
+        require('js/profile');
+        require('js/stats');
+        require('js/pulse');
+        require('js/info');
+        require('js/notify');
+        require('js/captcha');
+        require('js/menu');
+
         YAHOO.widget.Logger.enableBrowserConsole();
         YAHOO.lacuna.Game.Start(query);
         progressLoaderC.parentNode.removeChild(progressLoaderC);
@@ -490,7 +138,8 @@
     loader.onProgress = function(o) {
         if(firstFile) {
             if(this._combining) {
-                progressLoader.total = this.sorted.length - this._combining.length + 1; //remove the count of the files that are getting combined but still record it as 1 file
+                //remove the count of the files that are getting combined but still record it as 1 file
+                progressLoader.total = this.sorted.length - this._combining.length + 1;
             }
             else {
                 progressLoader.total = this.sorted.length;
@@ -500,7 +149,11 @@
         progressLoader.counter++;
         var perc = progressLoader.counter / progressLoader.total;
         progressLoader.style.width = Math.ceil(perc * progressLoaderC.offsetWidth) + "px";
-        progressLoader.innerHTML = [Math.round(perc*1000)/10, '% - ', progressLoader.counter < status.length ? status[progressLoader.counter] : o.name].join('');
+        progressLoader.innerHTML = [
+            Math.round(perc * 1000) / 10,
+            '% - ',
+            progressLoader.counter < status.length ? status[progressLoader.counter] : o.name
+        ].join('');
     };
     loader.onFailure = function(o) {
         YAHOO.log(o);
@@ -569,7 +222,6 @@
             'developing a theory',
             'making friends',
             'oxidizing lizards',
-            '<span style="background-color: #000000; color: #ffffff;">&nbsp;&nbsp;&nbsp;&nbsp;[ REDACTED ]&nbsp;&nbsp;&nbsp;</span>',
             'coercing automatons',
             'dissociating ions',
             'taking a break',
@@ -626,7 +278,6 @@
     progressLoader.innerHTML = status[progressLoader.counter];
 
     document.body.insertBefore(progressLoaderC, document.body.firstChild);
-
     loader.insert();
 })();
 // vim: noet:ts=4:sw=4
