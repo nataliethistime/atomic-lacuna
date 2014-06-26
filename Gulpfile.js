f3e50df960e2d265f4a895d2752c5b726ace3849
Vasari <nmccallum7@gmail.com>
2014-06-26 13:27:45 +1000

    Added (used on about.js) JSLint.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,11 +3,70 @@ var gutil      = require('gulp-util');
 
 var browserify = require('gulp-browserify');
 var cssConcat  = require('gulp-concat-css');
+var jslint     = require('gulp-jslint');
 
 var connect = require('connect');
 var http    = require('http');
 var path    = require('path');
 var fs      = require('fs');
+
+var JS_FILES = ['app/js/**/*.js'];
+
+gulp.task('lint', function() {
+    // console.log(gulp.src(JS_FILES));
+    // gulp.src(['app/js/*.js', 'app/js/building/*.js', 'app/js/module/*.js'])
+    //     .pipe(jshint({
+    //         bitwise : true,
+    //         curly   : true,
+    //         eqeqeq  : true,
+    //         es3     : false,
+    //         indent  : 4,
+    //         undef   : true,
+    //         unused  : true,
+
+    //         // Globals
+    //         yui     : true,
+    //         node    : true,
+    //         jquery  : true,
+    //         devel   : true,
+    //         browser : true,
+
+    //         // Hurt my feelings! I DARE you!!!
+    //         maxerr : Infinity
+
+    //         // TODO: do these at a later stage:
+    //         // es3 => true
+    //         // camelcase => true
+    //         // forin => true
+    //         // noempty => true
+    //         // plusplus => true
+    //         // quotmark => 'single'
+    //         // strict => true
+    //         // maxlen => 100
+    //     }))
+    //     .pipe(jshint.reporter('jshint-stylish'))
+    //     // Fail straight away if something goes wrong.
+    //     // .pipe(jshint.reporter('fail'));
+
+    gulp.src(JS_FILES)
+        .pipe(jslint({
+            // Globals
+            browser : true,
+            devel : true,
+            debug : false,
+            node : true,
+            // Lint options
+            maxerr : Infinity, // To infinity and beyond!
+            // maxlen : 100,
+            sloppy : false,
+            todo : true,
+            vars : true,
+
+            // Gulp options
+            reporter : 'default',
+            errorsOnly : true
+        }));
+});
 
 gulp.task('web-build', function() {
     gutil.log('Building Atomic Lacuna for in the browser.');
--- app/js/about.js
+++ app/js/about.js
@@ -1,97 +1,99 @@
+/*global YAHOO */
+'use strict';
+
 YAHOO.namespace("lacuna");
 
- 
-if (typeof YAHOO.lacuna.About == "undefined" || !YAHOO.lacuna.About) {
-    
-(function(){
+(function () {
     var Lang = YAHOO.lang,
         Util = YAHOO.util,
         Dom = Util.Dom,
-        Event = Util.Event,
-        Sel = Util.Selector,
         Lacuna = YAHOO.lacuna,
         Game = Lacuna.Game,
         Lib = Lacuna.Library;
-        
-    var About = function() {
+
+    var About = function () {
         this.id = "about";
         this.createEvent("onShow");
-        
+
         var container = document.createElement("div");
         container.id = this.id;
         Dom.addClass(container, "nofooter");
         Dom.addClass(container, Lib.Styles.HIDDEN);
-        container.innerHTML = this._getHtml();
+        container.innerHTML = this.html();
         document.body.insertBefore(container, document.body.firstChild);
-        
+
         this.Panel = new YAHOO.widget.Panel(this.id, {
-            constraintoviewport:true,
-            fixedcenter:true,
-            visible:false,
-            draggable:true,
-            effect:Game.GetContainerEffect(),
-            underlay:false,
-            modal:true,
-            close:true,
-            width:"450px",
-            zIndex:9999
+            constraintoviewport : true,
+            fixedcenter : true,
+            visible : false,
+            draggable : true,
+            effect : Game.GetContainerEffect(),
+            underlay : false,
+            modal : true,
+            close : true,
+            width : "450px",
+            zIndex : 9999
         });
-        this.Panel.renderEvent.subscribe(function(){
+
+        this.Panel.renderEvent.subscribe(function () {
             this.elCreditsList = Dom.get("aboutCredits");
             this.elVersion = Dom.get("aboutVersion");
-            
+
             Dom.removeClass(this.id, Lib.Styles.HIDDEN);
         }, this, true);
+
         this.Panel.render();
         Game.OverlayManager.register(this.Panel);
     };
     About.prototype = {
-        _getHtml : function() {
+        html : function () {
             return [
-            '    <div class="hd">About</div>',
-            '    <div class="bd">',
-            '        <div style="overflow:auto;height:400px">',
-            '            The Lacuna Expanse',
-            '            <ul>',
-            '                <li>&copy; 2011 by Lacuna Expanse Corp.</li>',
-            '                <li>Server Version: <span id="aboutVersion"></span></li>',
-            '            </ul><br/>',
-            '            Credits',
-            '            <ul id="aboutCredits">',
-            '            </ul>',
-            '        </div>',
-            '    </div>'
+                '<div class="hd">About</div>',
+                '<div class="bd">',
+                '    <div style="overflow:auto;height:400px">',
+                '        The Lacuna Expanse',
+                '        <ul>',
+                '            <li>&copy; 2011 by Lacuna Expanse Corp.</li>',
+                '            <li>Server Version: <span id="aboutVersion"></span></li>',
+                '        </ul><br/>',
+                '        Credits',
+                '        <ul id="aboutCredits">',
+                '        </ul>',
+                '    </div>',
+                '</div>'
             ].join('');
         },
-        show : function() {
-            if(!this.hasCredits) {
-                Game.Services.Stats.credits({},{
-                    success : function(o){
+        show : function () {
+            if (!this.hasCredits) {
+                Game.Services.Stats.credits({}, {
+                    success : function (o) {
                         YAHOO.log(o, "info", "Stats");
                         this.populateCredits(o.result);
                     },
-                    scope:this
+                    scope : this
                 });
             }
             this.elVersion.innerHTML = Game.ServerData.version;
             Game.OverlayManager.hideAll();
             this.Panel.show();
         },
-        hide : function() {
+        hide : function () {
             this.Panel.hide();
         },
-        
-        populateCredits : function(results) {
-            if(!this.hasCredits) {
+
+        populateCredits : function (results) {
+            // TODO: this method can use _ a lot!
+            var i, prop, x, obj, nLi, html;
+            if (!this.hasCredits) {
                 var list = this.elCreditsList,
                     li = document.createElement("li");
-                for(var i=0; i<results.length; i++) {
-                    var obj = results[i];
-                    for(var prop in obj) {
-                        if(obj.hasOwnProperty(prop)){
-                            var nLi = li.cloneNode(false),
-                                html = ["<label>",prop,"</label><ul>"];
-                            for(var x=0; x<obj[prop].length; x++) {
+                for (i = 0; i < results.length; i += 1) {
+                    obj = results[i];
+                    for (prop in obj) {
+                        if (obj.hasOwnProperty(prop)) {
+                            nLi = li.cloneNode(false);
+                            html = ["<label>", prop, "</label><ul>"];
+                            for (x = 0; x < obj[prop].length; x += 1) {
                                 html.push("<li>");
                                 html.push(obj[prop][x]);
                                 html.push("</li>");
@@ -106,13 +108,8 @@ if (typeof YAHOO.lacuna.About == "undefined" || !YAHOO.lacuna.About) {
                 this.Panel.center();
             }
         }
-        
     };
-    Lang.augmentProto(About, Util.EventProvider);
-            
-    Lacuna.About = new About();
-})();
-YAHOO.register("about", YAHOO.lacuna.About, {version: "1", build: "0"}); 
 
-}
-// vim: noet:ts=4:sw=4
+    Lang.augmentProto(About, Util.EventProvider);
+    Lacuna.About = new About();
+}());
